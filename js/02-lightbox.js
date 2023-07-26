c1d9e5fe6ea153f80299e223fd4bc2b64ddc54bc
Ancektor <ancektor@gmail.com>
2023-07-26 15:07:31 +0300

    26.07

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryItemTemplate = galleryItems
+	.map(
+		(item) => `<li class="gallery__item">
+   <a class="gallery__link" href="${item.original}">
+      <img class="gallery__image" src="${item.preview}" alt="${item.description}" width="480" />
+   </a>
+</li>`,
+	)
+	.join("");
 
-console.log(galleryItems);
+const galleryElem = document.querySelector(".gallery");
+galleryElem?.insertAdjacentHTML("beforeend", galleryItemTemplate);
+
+const gallery = new SimpleLightbox(".gallery a", {
+	captionsData: "alt",
+	captionDelay: 250,
+});
